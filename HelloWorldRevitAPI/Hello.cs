476fe1d02c5a42a076df6821a17881c7eaf9cd81
hari <hariharanradha098@gmail.com>
2024-02-13 07:49:04 +0530

    1st commit

---
+++ HelloWorldRevitAPI/Hello.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldRevitAPI
+{
+    public class Hello : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Title", "Vanakkam da Mapla");
+            return Result.Succeeded;
+        }
+    }
+}
